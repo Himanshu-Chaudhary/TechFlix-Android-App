b14d32c9c6f3c3fa5ae1fe4ae89ae61dd79bd195
Himanshu-Chaudhary <himanshu@unm.edu>
2019-08-22 21:28:43 -0600

    expanded view deflates

--- app/src/main/java/com/example/techrss/customAdapter.kt
+++ app/src/main/java/com/example/techrss/customAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.techrss
 
-import android.arch.lifecycle.ViewModelProvider
 import android.content.res.Resources
 import android.graphics.Bitmap
 import android.support.v7.widget.RecyclerView
@@ -9,11 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.Switch
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 
 data class viewData(val data: Triple<String,String,String>, var viewType: Int)
 
@@ -43,11 +39,16 @@ class NewsAdapter(val data: ArrayList<Item>, val resource : Resources) : Recycle
 
     override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
         val data = data[p1]
-        val listener: (View) -> Unit = {
+        val expander: (View) -> Unit = {
             this.expanded = p1
             notifyDataSetChanged()
         }
-        p0.itemView.setOnClickListener(listener)
+        val deflater: (View) -> Unit = {
+            this.expanded = -1
+            notifyDataSetChanged()
+        }
+
+        p0.itemView.setOnClickListener(expander)
         if (p0 is NewsViewHolder) {
             var doc = Jsoup.parse(data.content)
             var tempHtml = Html.fromHtml(data.content)
@@ -65,6 +66,7 @@ class NewsAdapter(val data: ArrayList<Item>, val resource : Resources) : Recycle
             p0.txtView2.text = data.title
             p0.txtView3.text = Html.fromHtml(data.content,ImageGetter(resource,p0.txtView3),null)
             p0.txtView4.text = data.link
+            p0.itemView.setOnClickListener(deflater)
         }
     }
 
